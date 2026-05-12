dbb54cc96ba38d065a9bd29ce81c78e9e3ea37cc
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-12 08:16:10 +0200

    Use translations in web manifest

--- src/pages/manifest.webmanifest.ts
+++ src/pages/manifest.webmanifest.ts
@@ -1,13 +1,15 @@
-import { siteConfig } from '../config/site';
+import { defaultLocale, siteConfig } from '../config/site';
+import { useTranslations } from '../i18n/ui';
 
 export function GET() {
   const basePath = import.meta.env.BASE_URL ?? siteConfig.base;
   const withBase = (path: string) => `${basePath.replace(/\/$/, '')}/${path.replace(/^\//, '')}`;
+  const t = useTranslations(defaultLocale);
 
   const manifest = {
     name: siteConfig.name,
     short_name: siteConfig.name,
-    description: siteConfig.description,
+    description: t('site.description'),
     start_url: basePath,
     display: 'standalone',
     background_color: '#ffffff',
