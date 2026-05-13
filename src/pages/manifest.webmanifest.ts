9342336aa7eb3008386d9a680b8644484eb9a71d
Jorge Alonso <jalonsomerchan@gmail.com>
2026-05-13 08:15:17 +0200

    Use shared path helpers in manifest

--- src/pages/manifest.webmanifest.ts
+++ src/pages/manifest.webmanifest.ts
@@ -1,22 +1,21 @@
 import { defaultLocale, siteConfig } from '../config/site';
 import { useTranslations } from '../i18n/ui';
+import { getBasePath, withBasePath } from '../utils/paths';
 
 export function GET() {
-  const basePath = import.meta.env.BASE_URL ?? siteConfig.base;
-  const withBase = (path: string) => `${basePath.replace(/\/$/, '')}/${path.replace(/^\//, '')}`;
   const t = useTranslations(defaultLocale);
 
   const manifest = {
     name: siteConfig.name,
     short_name: siteConfig.name,
     description: t('site.description'),
-    start_url: basePath,
+    start_url: getBasePath(),
     display: 'standalone',
     background_color: '#ffffff',
     theme_color: '#2563eb',
     icons: [
       {
-        src: withBase('favicon.svg'),
+        src: withBasePath('favicon.svg'),
         sizes: 'any',
         type: 'image/svg+xml',
       },
